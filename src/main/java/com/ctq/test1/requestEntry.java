4ad4ddfc470e751ed67330f209c82172e8aa2680
elhackman <hackman@live.com>
2013-05-26 15:40:12 -0500

    Final Version

--- src/main/java/com/ctq/test1/requestEntry.java
+++ src/main/java/com/ctq/test1/requestEntry.java
@@ -1,37 +1,37 @@
 package com.ctq.test1;
 
 public class requestEntry {
- 
+
 	private String Time;
 	private String Method;
 	private String Header;
 	private String Body;
-    
+
 	public requestEntry(String Method,String Time,String Header,String Body) {
 		this.Time = Time;
 		this.Method = Method;
 		this.Header = Header;
 		this.Body = Body;		
- 	}
-   
-	 public String getRtime() {
-		 return Time;
-	 }
+	}
 
-	 public String getRmethod() {
-		 return Method;
-	 }
-	 
-	 public String getRheader() {
-		 return Header;
-	 }
+	public String getRtime() {
+		return Time;
+	}
 
-	 public String getRbody() {
-		 return Body;
-	 }
-   
-  @Override
-  public String toString() {
-    return "requestEntry [time=" + Time + ", method=" + Method + ", header=" + Header + ", body=" + Body + "]";
-  } 
+	public String getRmethod() {
+		return Method;
+	}
+
+	public String getRheader() {
+		return Header;
+	}
+
+	public String getRbody() {
+		return Body;
+	}
+
+	@Override
+	public String toString() {
+		return "<tr><td style=\"width:120px\">" + Time + "</td><td>" + Method + "</td><td style=\"width:400px\">" + Header + "</td><td style=\"width:400px\">>" + Body + "</td></tr>";
+	} 
 }
